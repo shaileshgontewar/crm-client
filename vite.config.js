e2d3ead9a936c9be07f8d279bce010a021526a23
shailesh gontewar <shailesh.gontewar@tlminsidesales.com>
2025-11-05 19:14:09 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+  },
+});
